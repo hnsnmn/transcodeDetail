a512c4af4699bb739f75cae273a500fede64fc43
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 15:29:13 +0900

    # 결과를 통지하기 위한 협업 객체 추가

---
+++ src/test/java/org/hnsnmn/domain/transcode/JobResultNotifier.java
@@ -0,0 +1,12 @@
+package org.hnsnmn.domain.transcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 3:27
+ * To change this template use File | Settings | File Templates.
+ */
+public interface JobResultNotifier {
+	void notifyToRequester(Long jobId);
+}
