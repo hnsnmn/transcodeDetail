342901d2652b96a69eed66883edb35154f1d0383
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 15:25:29 +0900

    # 변환된 결과 파일과 썸네일 이미지를 목적지에 저장시킬 협업 객체 생성

---
+++ src/test/java/org/hnsnmn/domain/transcode/CreatedFileSender.java
@@ -0,0 +1,15 @@
+package org.hnsnmn.domain.transcode;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 3:23
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CreatedFileSender {
+	void send(List<File> multimediaFiles, List<File> thumbnails, Long jobId);
+}
