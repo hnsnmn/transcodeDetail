467868ba4991220bf0115a96a09660dece70ef79
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 14:41:14 +0900

    # 미디어 원본 파일을 로컬에 복사해주는 부분을 copyMultimediaSourceToLocal() 메서드가 처리하도록 하고, 이 메서드는 다시 mediaSourceCopier라는 객체의 copy()메서드에 위임하도록 코드를 작성

--- src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
+++ src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
@@ -3,6 +3,8 @@ package org.hnsnmn.domain.transcode;
 
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hongseongmin
@@ -12,12 +14,21 @@ import org.junit.Test;
  */
 public class TranscodingServiceTest {
 
+	private MediaSourceCopier mediaSourceCopier;
+
 	@Test
 	public void transcodeSuccessfully() {
 		// 미디어 원본으로부터 파일을 로컬에 복사한다.
+		Long jobId = new Long(1);
+		File multimediaFile = copyMultimediaSourceToLocal(jobId);
+
 		// 로컬에 복사된 파일을 변환처리 한다.
 		// 로컬에 복사된 파일로부터 이미지를 추출한다.
 		// 변환된 결과 파일과 썸네일 이미지를 목적지에 저장
 		// 결과를 통지
+	}
+
+	private File copyMultimediaSourceToLocal(Long jobId) {
+		return mediaSourceCopier.copy(jobId);
 	}
 }
