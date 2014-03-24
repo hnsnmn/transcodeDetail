866716d94eb9b37aa9fee39a9c2f9299902c83bb
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 14:28:30 +0900

    # transcodeSuccessfully() 메소드는 TranscodingService가 성공적으로 동작하는 상황을 테스트 하기 위해 만들었으며, 성공적으로 작업이 진행될 때 수행해야 할 작업을 주석으로 입력해 놓았다. 이 주석을 구현으로 바꿔치기 해 나가면서 TranscodingService를 점진적으로 완성해 나갈 것이다.

---
+++ src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
@@ -0,0 +1,23 @@
+package org.hnsnmn.domain.transcode;
+
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 2:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class TranscodingServiceTest {
+
+	@Test
+	public void transcodeSuccessfully() {
+		// 미디어 원본으로부터 파일을 로컬에 복사한다.
+		// 로컬에 복사된 파일을 변환처리 한다.
+		// 로컬에 복사된 파일로부터 이미지를 추출한다.
+		// 변환된 결과 파일과 썸네일 이미지를 목적지에 저장
+		// 결과를 통지
+	}
+}
