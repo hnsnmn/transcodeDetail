d2fd1ef2076f340d1625e51fe3c8e3d0a25a96ec
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 15:03:10 +0900

    # Transcoder 협업 객체 생성

---
+++ src/test/java/org/hnsnmn/domain/transcode/Transcoder.java
@@ -0,0 +1,15 @@
+package org.hnsnmn.domain.transcode;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 2:52
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Transcoder {
+	List<File> transcode(File mockMultimediaFile, Long jobId);
+}
