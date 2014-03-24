811150ea7e7c8c0919b852b79eeab65c06a355e1
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 14:49:19 +0900

    # mock을 활용해 mediaSourceCopier.copy 메소드가 호출되면 mockMultimediaFile을 반환하도록하고, 최종적으로 mediaSourceCopier.copy() 메소드가 호출했는지 검증한다.

--- src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
+++ src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
 
+import static org.mockito.Mockito.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hongseongmin
@@ -23,14 +25,19 @@ public class TranscodingServiceTest {
 
 	@Test
 	public void transcodeSuccessfully() {
-		// 미디어 원본으로부터 파일을 로컬에 복사한다.
 		Long jobId = new Long(1);
+		File mockMultimediaFile = mock(File.class);
+		when(mediaSourceCopier.copy(jobId)).thenReturn(mockMultimediaFile);
+
+		// 미디어 원본으로부터 파일을 로컬에 복사한다.
 		File multimediaFile = copyMultimediaSourceToLocal(jobId);
 
 		// 로컬에 복사된 파일을 변환처리 한다.
 		// 로컬에 복사된 파일로부터 이미지를 추출한다.
 		// 변환된 결과 파일과 썸네일 이미지를 목적지에 저장
 		// 결과를 통지
+
+		verify(mediaSourceCopier, only()).copy(jobId);
 	}
 
 	private File copyMultimediaSourceToLocal(Long jobId) {
