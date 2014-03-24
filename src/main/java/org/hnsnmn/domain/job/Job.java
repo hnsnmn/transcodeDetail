8011a3bceb5fd9c6802d58999ac1f1c732230dd1
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 18:03:26 +0900

    # Job과 JobRepository를 추가하며, jobRepository를 필드로 만들고 JobRepository 인터페이스를 생성하고 Job클래스도 함께 생성한다.

---
+++ src/main/java/org/hnsnmn/domain/job/Job.java
@@ -0,0 +1,23 @@
+package org.hnsnmn.domain.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 5:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class Job {
+	public boolean isSuccess() {
+		return false;  //To change body of created methods use File | Settings | File Templates.
+	}
+
+	public State isLastState() {
+		return null;  //To change body of created methods use File | Settings | File Templates.
+	}
+
+	public enum State {
+		MEDIASOURCECOPYING
+
+	}
+}
---
+++ src/main/java/org/hnsnmn/domain/job/JobRepository.java
@@ -0,0 +1,12 @@
+package org.hnsnmn.domain.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 24.
+ * Time: 오후 5:57
+ * To change this template use File | Settings | File Templates.
+ */
+public interface JobRepository {
+	Job findById(Long jobId);
+}
