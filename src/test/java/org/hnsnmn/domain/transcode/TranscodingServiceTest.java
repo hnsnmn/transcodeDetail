f31540339ffe9b8755b71b3c5afd2b0b1d1dd085
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-24 14:57:09 +0900

    # mediaSourceCopier.copy()와 동일하게 로컬에 복사된 파일을 변환처리할 경우 transcoder 객체에게 모든 기능을 위임 처리하며, 아직은 transcodingService구현에 집중해야 되므로 Mock객체를 사용한다.

--- src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
+++ src/test/java/org/hnsnmn/domain/transcode/TranscodingServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -22,6 +24,8 @@ public class TranscodingServiceTest {
 
 	@Mock
 	private MediaSourceCopier mediaSourceCopier;
+	@Mock
+	private Transcoder transcoder;
 
 	@Test
 	public void transcodeSuccessfully() {
@@ -29,15 +33,25 @@ public class TranscodingServiceTest {
 		File mockMultimediaFile = mock(File.class);
 		when(mediaSourceCopier.copy(jobId)).thenReturn(mockMultimediaFile);
 
+		List<File> mockMultimediaFiles = new ArrayList<File>();
+		when(transcoder.transcode(mockMultimediaFile, jobId)).thenReturn(mockMultimediaFiles);
+
 		// 미디어 원본으로부터 파일을 로컬에 복사한다.
 		File multimediaFile = copyMultimediaSourceToLocal(jobId);
 
 		// 로컬에 복사된 파일을 변환처리 한다.
+		List<File> multimediaFiles = transcode(multimediaFile, jobId);
+
 		// 로컬에 복사된 파일로부터 이미지를 추출한다.
 		// 변환된 결과 파일과 썸네일 이미지를 목적지에 저장
 		// 결과를 통지
 
 		verify(mediaSourceCopier, only()).copy(jobId);
+		verify(transcoder, only()).transcode(mockMultimediaFile, jobId);
+	}
+
+	private List<File> transcode(File multimediaFile, Long jobId) {
+		return transcoder.transcode(multimediaFile, jobId);
 	}
 
 	private File copyMultimediaSourceToLocal(Long jobId) {
