9f668830b9383a7538ddf39e836ede871a3ab181
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-26 10:43:50 +0900

    # 비디오 변환을 처리하면 변환된 결과 비디오 파일과 썸네일 파일이 생성되는데, 그 파일을 작업 요청자가 지정한 위치에 저장해야한다. 이 위치는 FTP가 될 수도 있고, HTTP 파일 업로드가 될수도 있고, 단순히 로컬 파일 시스템이 될 수 도있다. 일단, 구현의 상세함은 뒤로 미루고 결과 파일이 보관 될 위치를 추상화 인터페이스를 생성한다.

---
+++ src/main/java/org/hnsnmn/domain/job/DestinationStorage.java
@@ -0,0 +1,11 @@
+package org.hnsnmn.domain.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 26.
+ * Time: 오전 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DestinationStorage {
+}
