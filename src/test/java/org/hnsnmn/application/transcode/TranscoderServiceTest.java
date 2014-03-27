ea0b14b61707ea68168996d93991ab59352bdaa0
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-27 14:26:18 +0900

    # 지금까지 TranscodingService부터 시작해서 Job까지 점진적으로 구현을 만들어 왔다. 다음 작업 대상은 Job객체에서 호출할 Transcoder의 구현체를 만들 것이다.

---
+++ src/test/java/org/hnsnmn/application/transcode/TranscoderServiceTest.java
@@ -0,0 +1,11 @@
+package org.hnsnmn.application.transcode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 3. 27.
+ * Time: 오후 2:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class TranscoderServiceTest {
+}
