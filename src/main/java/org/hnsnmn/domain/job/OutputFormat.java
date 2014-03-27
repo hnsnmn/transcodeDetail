4bb79ff639e6c225a55a54108ce063e1f27230ce
{hongseongmin} <{hnsnmn@gmail.com}>
2014-03-27 14:37:14 +0900

    # OutputFormat 생성자 추가

--- src/main/java/org/hnsnmn/domain/job/OutputFormat.java
+++ src/main/java/org/hnsnmn/domain/job/OutputFormat.java
@@ -7,5 +7,18 @@ package org.hnsnmn.domain.job;
  * Time: 오후 2:05
  * To change this template use File | Settings | File Templates.
  */
-public interface OutputFormat {
+public class OutputFormat {
+	private int width;
+	private int height;
+	private int bitrate;
+	private String videoFormat;
+	private String audioFormat;
+
+	public OutputFormat(int width, int height, int bitrate, String videoFormat, String audioFormat) {
+		this.width = width;
+		this.height = height;
+		this.bitrate = bitrate;
+		this.videoFormat = videoFormat;
+		this.audioFormat = audioFormat;
+	}
 }
