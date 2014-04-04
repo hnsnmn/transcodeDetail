73ee4bca783b791c4f54110b77bfc8ec1cfa1e77
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-04 17:36:28 +0900

    # 확장자를 가져오는 부분은 container 객체에 위임함.

--- src/main/java/org/hnsnmn/domain/job/OutputFormat.java
+++ src/main/java/org/hnsnmn/domain/job/OutputFormat.java
@@ -11,13 +11,15 @@ public class OutputFormat {
 	private int width;
 	private int height;
 	private int bitrate;
+	private Container container;
 	private VideoCodec videoCodec;
 	private AudioCodec audioCodec;
 
-	public OutputFormat(int width, int height, int bitrate, VideoCodec videoCodec, AudioCodec audioCodec) {
+	public OutputFormat(int width, int height, int bitrate, Container container, VideoCodec videoCodec, AudioCodec audioCodec) {
 		this.width = width;
 		this.height = height;
 		this.bitrate = bitrate;
+		this.container = container;
 		this.videoCodec = videoCodec;
 		this.audioCodec = audioCodec;
 	}
@@ -43,6 +45,6 @@ public class OutputFormat {
 	}
 
 	public String getFileExtenstion() {
-		return "mp4";
+		return container.getFileExtenstion();
 	}
 }
