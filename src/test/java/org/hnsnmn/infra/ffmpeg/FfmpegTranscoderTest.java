f6fa579c008bf934038dc26392dd665fe3517099
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-04 13:53:45 +0900

    # 테스트 실패 원인은 FfmpegTranscoder#transcode() 메서드에서 실제 파일을 리턴해 주어야 한다. 뿐만아니라 폭, 높이 등에 대한 검증을 통과하려면 실제 비디오 파일을 생성해서 리턴해 주어야한다. 현재 테스트를 성공시키기 위해서는 비디오 변환 기능이 필요하다.

--- src/test/java/org/hnsnmn/infra/ffmpeg/FfmpegTranscoderTest.java
+++ src/test/java/org/hnsnmn/infra/ffmpeg/FfmpegTranscoderTest.java
@@ -1,5 +1,9 @@
 package org.hnsnmn.infra.ffmpeg;
 
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
 import org.hnsnmn.domain.job.OutputFormat;
 import org.hnsnmn.domain.job.Transcoder;
 import org.junit.Before;
@@ -30,12 +34,46 @@ public class FfmpegTranscoderTest {
 
 	@Test
 	public void transcodeWithOnOutputFormat() {
-		File multimediaFile = new File(".");
+		File multimediaFile = new File("src/test/resources/sample.avi");
 		List<OutputFormat> outputFormats = new ArrayList<OutputFormat>();
 		outputFormats.add(new OutputFormat(640, 480, 300, "h264", "aac"));
 		List<File> transcodeFiles = transcoder.transcode(multimediaFile, outputFormats);
 
 		assertEquals(1, transcodeFiles.size());
 		assertTrue(transcodeFiles.get(0).exists());
+		verifyTranscodedFile(outputFormats.get(0), transcodeFiles.get(0));
+	}
+
+	private void verifyTranscodedFile(OutputFormat outputFormat, File file) {
+		IContainer container = IContainer.make();
+		int openResult = container.open(file.getAbsolutePath(), IContainer.Type.READ, null);
+		if (openResult < 0) {
+			throw new RuntimeException("Xuggler file open failed:" + openResult);
+		}
+		int numStreams = container.getNumStreams();
+
+		int width = 0;
+		int height = 0;
+		ICodec.ID videoCodec = null;
+		ICodec.ID audioCodec = null;
+
+		for (int i = 0; i < numStreams; i++) {
+			IStream stream = container.getStream(i);
+			IStreamCoder coder = stream.getStreamCoder();
+
+			if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO) {
+				audioCodec = coder.getCodecID();
+			} else if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+				videoCodec = coder.getCodecID();
+				width = coder.getWidth();
+				height = coder.getHeight();
+			}
+		}
+		container.close();
+
+		assertEquals(outputFormat.getWidth(), width);
+		assertEquals(outputFormat.getHeight(), height);
+		assertEquals(outputFormat.getVideoFormat(), videoCodec.toString());
+		assertEquals(outputFormat.getAudioFormat(), audioCodec.toString());
 	}
 }
