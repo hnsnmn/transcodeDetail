0b2b1eb42b183b0404fb3ed6a251fb6713d588c8
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-04 14:33:33 +0900

    # 의미를 조금더 명확하게 하기 위해 Enum 타입 추가

---
+++ src/main/java/org/hnsnmn/domain/job/VideoCodec.java
@@ -0,0 +1,12 @@
+package org.hnsnmn.domain.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 4.
+ * Time: 오후 2:28
+ * To change this template use File | Settings | File Templates.
+ */
+public enum VideoCodec {
+	H264
+}
