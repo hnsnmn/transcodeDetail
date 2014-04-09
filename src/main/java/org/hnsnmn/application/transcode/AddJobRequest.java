a60b045198bb5ffcfb5bceb058200d623aad4117
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-09 15:00:28 +0900

    # AddJobRequest 구현 - 하지만 아직까지는 set 해주는 곳을 구현하지 않음. - 어디서 injecting 해줘야 되나??

--- src/main/java/org/hnsnmn/application/transcode/AddJobRequest.java
+++ src/main/java/org/hnsnmn/application/transcode/AddJobRequest.java
@@ -12,19 +12,48 @@ import java.util.List;
 * To change this template use File | Settings | File Templates.
 */
 public class AddJobRequest {
+	private String mediaSource;
+	private String destinationStorage;
+	private String resultCallback;
+	private List<OutputFormat> outputFormats;
+
+	public void setMediaSource(String mediaSource) {
+		this.mediaSource = mediaSource;
+	}
+
+	public void setDestinationStorage(String destinationStorage) {
+		this.destinationStorage = destinationStorage;
+	}
+
+	public void setOutputFormats(List<OutputFormat> outputFormats) {
+		this.outputFormats = outputFormats;
+	}
+
+	public void setResultCallback(String resultCallback) {
+		this.resultCallback = resultCallback;
+	}
+
 	public String getMediaSource() {
-		return null;  //To change body of created methods use File | Settings | File Templates.
+		return mediaSource;
 	}
 
 	public String getDestinationStorage() {
-		return null;  //To change body of created methods use File | Settings | File Templates.
+		return destinationStorage;
 	}
 
 	public List<OutputFormat> getOutputFormats() {
-		return null;  //To change body of created methods use File | Settings | File Templates.
+		return outputFormats;
 	}
 
 	public String getResultCallback() {
-		return null;
+		return resultCallback;
+	}
+
+	@Override
+	public String toString() {
+		return "AddJobRequest [mediaSource=" + mediaSource
+				+ ", destinationStorage=" + destinationStorage
+				+ ", outputFormats=" + outputFormats
+				+ ", resultCallback=" + resultCallback + "]";
 	}
 }
