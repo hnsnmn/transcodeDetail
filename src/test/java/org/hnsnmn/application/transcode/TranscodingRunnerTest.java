2bb6c058620d09b4e4c3f1fe3a956aabc7201063
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-09 17:15:35 +0900

    # dontRunTranscodingWhenJobIsNotExists() 메서드 구현

--- src/test/java/org/hnsnmn/application/transcode/TranscodingRunnerTest.java
+++ src/test/java/org/hnsnmn/application/transcode/TranscodingRunnerTest.java
@@ -3,14 +3,14 @@ package org.hnsnmn.application.transcode;
 
 import org.hnsnmn.domain.job.Job;
 import org.hnsnmn.domain.job.JobRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.only;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,14 +28,28 @@ public class TranscodingRunnerTest {
 	@Mock
 	private JobRepository jobRepository;
 
+	private TranscodingRunner runner;
+
+	@Before
+	public void setUp() {
+		runner = new TranscodingRunner(transcodingService, jobRepository);
+	}
+
 	@Test
 	public void runTranscodingWhenSuccessfullyWhenJobIsExists() {
-		TranscodingRunner runner = new TranscodingRunner(transcodingService, jobRepository);
 		when(jobRepository.findEldestJobOfcreatedState()).thenReturn(job);
 		when(job.getId()).thenReturn(1L);
 
 		runner.run();
 		verify(transcodingService, only()).transcode(1L);
+	}
+
+	@Test
+	public void dontRunTranscodingWhenJobIsNotExists() {
+		when(jobRepository.findEldestJobOfcreatedState()).thenReturn(null);
+		runner.run();
+
+		verify(transcodingService, never()).transcode(anyLong());
 	}
 
 }
