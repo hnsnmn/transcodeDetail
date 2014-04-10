639225eaf75a146b6751750cbe7b48771bf590be
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-10 13:22:40 +0900

    # FileDestinationStorage 구현체 작성 - 근데 구현체 작성은 했는데 언제 테스트를 해봐야 되나...

--- src/main/java/org/hnsnmn/domain/job/destination/FileDestinationStorage.java
+++ src/main/java/org/hnsnmn/domain/job/destination/FileDestinationStorage.java
@@ -2,7 +2,7 @@ package org.hnsnmn.domain.job.destination;
 
 import org.hnsnmn.domain.job.DestinationStorage;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -14,12 +14,61 @@ import java.util.List;
  */
 public class FileDestinationStorage extends DestinationStorage {
 
-	public FileDestinationStorage(String folder) {
-		super(folder);
+	public FileDestinationStorage(String url) {
+		super(url);
 	}
 
 	@Override
 	public void save(List<File> multimediaFiles, List<File> thumbnails) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		try {
+			copy(multimediaFiles, getUrl());
+		} catch (IOException ex) {
+			throw new RuntimeException("Fail to copy: " + ex.getMessage(), ex);
+		}
+
+	}
+
+	private void copy(List<File> files, String folder) throws IOException {
+		for (File file : files) {
+			copy(file, folder);
+		}
+	}
+
+	private void copy(File source, String folder) throws IOException {
+		String fileName = getFileName(source);
+		File target = new File(folder, fileName);
+		copy(source, target);
+	}
+
+	private String getFileName(File source) {
+		return source.getName();
+	}
+
+	private void copy(File source, File target) throws IOException {
+		BufferedInputStream is = null;
+		BufferedOutputStream os = null;
+
+		try {
+			is = new BufferedInputStream(new FileInputStream(source));
+			os = new BufferedOutputStream(new FileOutputStream(target));
+			byte[] data = new byte[8096];
+			int len = -1;
+			while ((len = is.read(data)) != -1) {
+				os.write(data, 0, len);
+			}
+
+		} finally {
+			closeStream(is);
+			closeStream(os);
+		}
+	}
+
+	private void closeStream(Closeable closeable) {
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException e) {
+			}
+		}
 	}
 }
