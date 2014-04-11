c824a4edb1e0a3dcebffb4cac4df7ba996c35bed
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 15:50:57 +0900

    # JpaConfig 작성 - setPersistenceUnitName은 META-INF에 저장된 persistence.xml에 등록되어 있다.

--- src/main/java/org/hnsnmn/springconfig/JpaConfig.java
+++ src/main/java/org/hnsnmn/springconfig/JpaConfig.java
@@ -1,6 +1,17 @@
 package org.hnsnmn.springconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,4 +22,34 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class JpaConfig {
+	@Autowired
+	private DataSource dataSource;
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcesso() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPersistenceUnitName("hnsnmn");
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		return factoryBean;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
+		return hibernateJpaVendorAdapter;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+		return jpaTransactionManager;
+	}
 }
