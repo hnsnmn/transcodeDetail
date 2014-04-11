8c8bbd7b8a901459f137cb0ba41a6faed551b2c1
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 15:49:37 +0900

    # 생성자를 통해서 받은 의존 객체와 디비에서 조회해온 job 데이터 정보를 이용해 Job 객체를 생성한다. 추가적으로 JobImpl 클래스에서 Job객체를 생성하도록 위임

--- src/main/java/org/hnsnmn/springconfig/JpaJobRepository.java
+++ src/main/java/org/hnsnmn/springconfig/JpaJobRepository.java
@@ -1,7 +1,17 @@
 package org.hnsnmn.springconfig;
 
+import org.hnsnmn.application.transcode.DestinationStorageFactory;
+import org.hnsnmn.application.transcode.MediaSourceFileFactory;
+import org.hnsnmn.application.transcode.ResultCallbackFactory;
 import org.hnsnmn.domain.job.Job;
 import org.hnsnmn.domain.job.JobRepository;
+import org.hnsnmn.infra.repositories.JobData;
+import org.hnsnmn.infra.repositories.JobImpl;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,10 +20,35 @@ import org.hnsnmn.domain.job.JobRepository;
  * Time: 오전 11:06
  * To change this template use File | Settings | File Templates.
  */
+@Repository
 public class JpaJobRepository implements JobRepository {
+
+	private final MediaSourceFileFactory mediaSourceFileFactory;
+	private final DestinationStorageFactory destinationStorageFactory;
+	private final ResultCallbackFactory resultCallbackFactory;
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public JpaJobRepository(MediaSourceFileFactory mediaSourceFileFactory,
+							DestinationStorageFactory destinationStorageFactory,
+							ResultCallbackFactory resultCallbackFactory) {
+		this.mediaSourceFileFactory = mediaSourceFileFactory;
+		this.destinationStorageFactory = destinationStorageFactory;
+		this.resultCallbackFactory = resultCallbackFactory;
+	}
+
+	@Transactional
 	@Override
 	public Job findById(Long jobId) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		JobData jobData = entityManager.find(JobData.class, jobId);
+		if (jobData == null) {
+			return null;
+		}
+		return createJobFromJobData(jobData);
+	}
+
+	private Job createJobFromJobData(JobData jobData) {
+		return new JobImpl(jobData, mediaSourceFileFactory, destinationStorageFactory, resultCallbackFactory);
 	}
 
 	@Override
