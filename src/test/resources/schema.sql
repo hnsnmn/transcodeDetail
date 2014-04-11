bd6fa36a075f9e0c178998f40c2fb958c27b55b6
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 15:37:07 +0900

    # 테스트를 진행하기 위해서(JpaJobRepositoryIntTest#findById) 임시 데이터 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,8 +1,9 @@
 create table JOB (
-	JOB_ID INT IDENTITY,
+	JOB_ID integer,
 	STATE varchar(20),
 	SOURCE_URL varchar(100),
 	DESTINATION_URL varchar(100),
 	CALLBACK_URL varchar(100),
-	EXCEPTION_MESSAGE varchar(255)
+	EXCEPTION_MESSAGE varchar(255),
+	primary key (JOB_ID)
 );
