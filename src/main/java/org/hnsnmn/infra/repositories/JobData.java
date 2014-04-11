f833cb9da64c073c9a872f9b86051584037b306b
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 17:06:10 +0900

    # db table과 매핑되는 Entity객체 생성

---
+++ src/main/java/org/hnsnmn/infra/repositories/JobData.java
@@ -0,0 +1,65 @@
+package org.hnsnmn.infra.repositories;
+
+import org.hnsnmn.domain.job.Job;
+import org.hnsnmn.domain.job.OutputFormat;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "JOB")
+public class JobData {
+
+	@Id
+	@Column(name = "JOB_ID")
+	private Long id;
+
+	@Column(name = "STATE")
+	@Enumerated(EnumType.STRING)
+	private Job.State state;
+
+	@Column(name = "SOURCE_URL")
+	private String sourceUrl;
+
+	@Column(name = "DESTINATION_URL")
+	private String destinationUrl;
+
+	@Column(name = "CALLBACK_URL")
+	private String callbackUrl;
+
+	@Column(name = "EXCEPTION_MESSAGE")
+	private String exceptionMessage;
+
+	@Transient
+	private List<OutputFormat> outputFormats;
+
+	public Long getId() {
+		return id;
+	}
+
+	public Job.State getState() {
+		return state;
+	}
+
+	public String getSourceUrl() {
+		return sourceUrl;
+	}
+
+	public String getDestinationUrl() {
+		return destinationUrl;
+	}
+
+	public String getCallbackUrl() {
+		return callbackUrl;
+	}
+
+	public String getExceptionMessage() {
+		return exceptionMessage;
+	}
+
+	public List<OutputFormat> getOutputFormats() {
+		return outputFormats;
+	}
+
+}
+
