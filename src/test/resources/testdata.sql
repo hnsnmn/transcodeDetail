aa67951b4f17d42065ca969d36069dc113f7955e
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 18:13:02 +0900

    # JOB_OUTPUTFORMAT 등록할 임시 데이터

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,1 +1,3 @@
 insert into JOB values (1, 'WAITING', 'file://source.avi', 'file://dest', 'http://calback', null);
+insert into JOB_OUTPUTFORMAT values (1, 0, 10, 20, 30, 'MP4', 'H264', 'AAC');
+insert into JOB_OUTPUTFORMAT values (1, 1, 100, 200, 300, 'AVI', 'MPEG4', 'MP3');
