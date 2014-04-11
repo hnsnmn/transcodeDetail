b5f9d7398552fdb959acc9b8e94363c49a232f4c
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-11 18:12:41 +0900

    # JOB_OUTPUTFORMAT ddl문 그리고 INDEX 문 추가 작성

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,9 +1,22 @@
 create table JOB (
-	JOB_ID integer,
-	STATE varchar(20),
-	SOURCE_URL varchar(100),
-	DESTINATION_URL varchar(100),
-	CALLBACK_URL varchar(100),
-	EXCEPTION_MESSAGE varchar(255),
+	JOB_ID              INT IDENTITY,
+	STATE               varchar(20),
+	SOURCE_URL          varchar(100),
+	DESTINATION_URL     varchar(100),
+	CALLBACK_URL        varchar(100),
+	EXCEPTION_MESSAGE   varchar(255),
 	primary key (JOB_ID)
 );
+
+create table JOB_OUTPUTFORMAT (
+  JOB_ID      INT,
+  LIST_IDX    INT,
+  WIDTH       INT,
+  HEIGHT      INT,
+  BITRATE     INT,
+  CONTAINER     varchar(20),
+  VIDEO_CODEC   varchar(20),
+  AUDIO_CODEC   varchar(20)
+);
+
+create index JOB_OUTPUTFORMAT_IDX on JOB_OUTPUTFORMAT (JOB_ID, LIST_IDX);
