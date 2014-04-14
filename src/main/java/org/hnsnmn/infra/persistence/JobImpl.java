1ad1f5c53224526568f37a9eb69c047c72f99dac
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-14 22:36:51 +0900

    # chnageState() 메서드에서 jobDataDao를 이용해 디비에 저장하기 위해 생성자를 통해서 객체를 할당받는다.

---
+++ src/main/java/org/hnsnmn/infra/persistence/JobImpl.java
@@ -0,0 +1,34 @@
+package org.hnsnmn.infra.persistence;
+
+import org.hnsnmn.domain.job.*;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 4. 11.
+ * Time: 오후 3:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobImpl extends Job {
+	private JobDataDao jobDataDao;
+
+	public JobImpl(JobDataDao jobDataDao, Long id,
+				   State state,
+				   MediaSourceFile mediaSourceFile,
+				   DestinationStorage destinationStorage,
+				   List<OutputFormat> outputFormats,
+				   ResultCallback callback,
+				   String errorMessage) {
+		super(id, state,mediaSourceFile, destinationStorage, outputFormats,
+				callback, errorMessage);
+		this.jobDataDao = jobDataDao;
+	}
+
+	@Override
+	protected void changeState(State newState) {
+		super.changeState(newState);
+	}
+
+}
