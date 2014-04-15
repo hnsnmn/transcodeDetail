3939069e48b5f4adfdc6ac73aa4f7bd26216ba51
{hongseongmin} <{hnsnmn@gmail.com}>
2014-04-15 11:13:47 +0900

    # AddJobService, TranscodingService 빈 객체 등록 - jobQueue는 Bean을 따로 등록하지 않고 MemoryJobQueue를 생성한다.

--- src/main/java/org/hnsnmn/springconfig/TranscodeApplicationConfig.java
+++ src/main/java/org/hnsnmn/springconfig/TranscodeApplicationConfig.java
@@ -1,5 +1,12 @@
 package org.hnsnmn.springconfig;
 
+import org.hnsnmn.application.transcode.*;
+import org.hnsnmn.domain.job.JobRepository;
+import org.hnsnmn.domain.job.ThumbnailExtractor;
+import org.hnsnmn.domain.job.Transcoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hongseongmin
@@ -8,4 +15,33 @@ package org.hnsnmn.springconfig;
  * To change this template use File | Settings | File Templates.
  */
 public class TranscodeApplicationConfig {
+
+	@Autowired
+	private MediaSourceFileFactory mediaSourceFileFactory;
+	@Autowired
+	private DestinationStorageFactory destinationStorageFactory;
+	@Autowired
+	private ResultCallbackFactory resultCallbackFactory;
+	@Autowired
+	private JobRepository jobRepository;
+	@Autowired
+	private Transcoder transcoder;
+
+	private ThumbnailExtractor thumbnailExtractor;
+
+	@Bean
+	public AddJobService addJobService() {
+		return new AddJobServiceImpl(mediaSourceFileFactory,
+				destinationStorageFactory, resultCallbackFactory,
+				jobRepository, jobQueue());
+	}
+
+	@Bean
+	public TranscodingService transcodingService() {
+		return new TranscodingServiceImple(transcoder, thumbnailExtractor, jobRepository);
+	}
+
+	public JobQueue jobQueue() {
+		return new MemoryJobQueue();
+	}
 }
